15b8ef1835a45247de82f80b46b29ee20a6858e5
lorenzo medici <lorenzomedici2001@gmail.com>
2022-09-08 12:34:37 +0200

    version uncomplete

--- app/src/main/java/dmc66/com/FamilyActivity.java
+++ app/src/main/java/dmc66/com/FamilyActivity.java
@@ -2,12 +2,32 @@ package dmc66.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
+
+    /** Handles playback of all the sound files */
+
+    private MediaPlayer mMediaPlayer;
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +38,12 @@ public class FamilyActivity extends AppCompatActivity {
         // Create a list of words
 
         ArrayList<Word> numberArrayList = new ArrayList<>();
-        numberArrayList.add(new Word(R.drawable.dad,"الاب","Father",R.raw.rolling));
-        numberArrayList.add(new Word(R.drawable.mother,"الام","Mother",R.raw.rolling));
-        numberArrayList.add(new Word(R.drawable.brother,"الاخ","Brother",R.raw.rolling));
-        numberArrayList.add(new Word(R.drawable.sister,"الاخت","Sister",R.raw.rolling));
-        numberArrayList.add(new Word(R.drawable.grandpa,"الجد","Grandfather",R.raw.rolling));
-        numberArrayList.add(new Word(R.drawable.grandma,"الجدة","Grandmother",R.raw.rolling));
+        numberArrayList.add(new Word(R.drawable.dad,"الاب","Father",R.raw.father));
+        numberArrayList.add(new Word(R.drawable.mother,"الام","Mother",R.raw.mother));
+        numberArrayList.add(new Word(R.drawable.brother,"الاخ","Brother",R.raw.bro));
+        numberArrayList.add(new Word(R.drawable.sister,"الاخت","Sister",R.raw.sis));
+        numberArrayList.add(new Word(R.drawable.grandpa,"الجد","Grandfather",R.raw.grandpa));
+        numberArrayList.add(new Word(R.drawable.grandma,"الجدة","Grandmother",R.raw.grandma));
 
 
         /** Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
@@ -43,7 +63,57 @@ public class FamilyActivity extends AppCompatActivity {
          *  {@link ListView} will display list items for each {@link Word} in the list.
          */
         listView.setAdapter(items);
+        // Set a click listener to play the audio when the list item is clicked on
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
 
+                // Get the {@link Word} object at the given position the user clicked on
+                Word word = numberArrayList.get(position);
+
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+                mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getResourceAudioId());
+
+                // Start the audio file
+                mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
+
+
 }
+
 
