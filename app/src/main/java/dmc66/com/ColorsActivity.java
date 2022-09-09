8b9a2bfc663500d0443072847e2c0f36cc1354a8
lorenzo medici <lorenzomedici2001@gmail.com>
2022-09-09 17:33:43 +0200

    version uncomplete-1.2.2

--- app/src/main/java/dmc66/com/ColorsActivity.java
+++ app/src/main/java/dmc66/com/ColorsActivity.java
@@ -2,12 +2,33 @@ package dmc66.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
+    /** Handles playback of all the sound files */
+
+    private MediaPlayer mMediaPlayer;
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +67,56 @@ public class ColorsActivity extends AppCompatActivity {
          *  {@link ListView} will display list items for each {@link Word} in the list.
          */
         listView.setAdapter(items);
+        // Set a click listener to play the audio when the list item is clicked on
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
 
+                // Get the {@link Word} object at the given position the user clicked on
+                Word word = numberArrayList.get(position);
+                Log.v("FamilyActivity", "Current word: " + word);
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+                mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getResourceAudioId());
+
+                // Start the audio file
+                mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
+
+
 }
+
